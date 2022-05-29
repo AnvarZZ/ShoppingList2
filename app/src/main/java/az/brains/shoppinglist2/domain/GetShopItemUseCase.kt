1f08cbab6a92e2c99703f7f84d7715cf1c999370
AnvarZZ <anvarzeynalzade@gmail.com>
2022-05-29 12:56:51 +0400

    Add data package

--- app/src/main/java/az/brains/shoppinglist2/domain/GetShopItemUseCase.kt
+++ app/src/main/java/az/brains/shoppinglist2/domain/GetShopItemUseCase.kt
@@ -2,7 +2,7 @@ package az.brains.shoppinglist2.domain
 
 class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
-    fun getShopItem(shopItemId: Int) {
-        shopListRepository.getShopItem(shopItemId)
+    fun getShopItem(shopItemId: Int):ShopItem {
+        return shopListRepository.getShopItem(shopItemId)
     }
 }
